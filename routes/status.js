660c6b4311c00a4e410b7b2b9ffa8cd8a97dec5d
Okwrighty <33523480+Okwrighty@users.noreply.github.com>
2025-11-21 12:52:12 -0500

    Delete routes directory

--- routes/status.js
+++
@@ -1,5 +0,0 @@
-// routes/status.js
-const express = require('express');
-const router = express.Router();
-router.get('/', (req, res) => res.json({ status: 'ok', timestamp: Date.now() }));
-module.exports = router;
